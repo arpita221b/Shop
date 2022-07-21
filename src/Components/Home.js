55501e70e1e90f74c4a5f43cdea8486e081d8e27
arpita221b <navalarpita@gmail.com>
2022-07-21 16:54:55 +0530

    Remove unecessary imports

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext } from "react";
 import { Data } from "../App";
 import Card from "./Card";
 import { v4 as uuidv4 } from "uuid";
